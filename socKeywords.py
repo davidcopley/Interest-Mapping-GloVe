685fb35bcd822f7f620b7dba7f41c6f5a932d9e1
David Copley <davidnicholascopley@hotmail.com>
2018-01-22 19:40:32 +1100

    remove nltk dependencies

--- socKeywords.py
+++ socKeywords.py
@@ -1,68 +1,106 @@
+
+# coding: utf-8
+
+# In[157]:
+
+
 import pandas as pd
+from spacy.tokenizer import Tokenizer
 import spacy
-from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
-import string
+nlp = spacy.load('en_core_web_lg')
+tokenizer = Tokenizer(nlp.vocab)
+from spacy.lang.en.stop_words import STOP_WORDS
 import numpy as np
-from sklearn.metrics.pairwise import cosine_similarity
+
+
+# In[184]:
+
 
 df = pd.read_excel("./soc_2018_definitions.xlsx")
+
+
+# In[185]:
+
+
 df = df.dropna()
 df = df.reset_index()
-df = df[['Unnamed: 2', 'Unnamed: 3']]
-df.columns = ['title', 'desc']
+df = df[['Unnamed: 2','Unnamed: 3']]
+df.columns = ['title','desc']
 df = df.drop(df.index[0])
-
-nlp = spacy.load('en_core_web_lg')
-topic_labels = df['title'].values
-stopset = set(stopwords.words('english'))
+# display(df)
 
 
+# In[186]:
+
+
+import string
 def removeStopwords(t):
-    print(t)
-    t = "".join((char.lower() for char in t if char not in string.punctuation))
-    tokens = word_tokenize(t)
-    tokens = [w for w in tokens if not w in stopset]
-    return " ".join(tokens)
+    t = "".join(char for char in t if char not in string.punctuation)
+    t = [token.orth_ for token in tokenizer(t) if token.orth_ not in STOP_WORDS and not token.is_punct and not token.is_space]
+    return " ".join(char for char in t if char not in string.punctuation)
+
+
+# In[187]:
+
 
 df['desc'] = df['desc'].apply(removeStopwords)
+display(df)
+
+
+# In[188]:
+
 
 topic_keywords = df['desc'].values
+
+
+# In[189]:
+
+
 topic_docs = list(nlp.pipe(topic_keywords,
-                           batch_size=10000,
-                           n_threads=3))
+  batch_size=10000,
+  n_threads=3))
 
-topic_vectors = np.array([doc.vector
-                          if doc.has_vector else spacy.vocab[0].vector
-                          for doc in topic_docs])
 
+# In[225]:
+
+
+topic_vectors = np.array([doc.vector if doc.has_vector else spacy.vocab[0].vector for doc in topic_docs])
+
+
+# In[229]:
 
 
 keywords = [
-    'data',
-    'rubbish',
-    'teenagers',
-    'law',
-    'world of warcraft',
-    'pedophile',
-    'bachelor of science',
-    'java',
-    'games developer'
+    'rectum'
 ]
 keyword_docs = list(nlp.pipe(keywords,
-                             batch_size=10000,
-                             n_threads=3))
+  batch_size=10000,
+  n_threads=3))
 keyword_vectors = np.array([doc.vector
-                            if doc.has_vector else spacy.vocab[0].vector
-                            for doc in keyword_docs])
+  if doc.has_vector else spacy.vocab[0].vector
+  for doc in keyword_docs])
+# print('Vector for keyword “%s”: ' % keywords[0])
+# print(keyword_vectors[0])
 
+
+# In[230]:
+
+
+from sklearn.metrics.pairwise import cosine_similarity
+# use numpy and scikit-learn vectorized implementations for performance
 simple_sim = cosine_similarity(keyword_vectors, topic_vectors)
-test = np.argsort(-simple_sim, axis=1)
-topic_idx = test
+topic_idx = np.argsort(-simple_sim,axis=1)
+# print(simple_sim)
+
+
+# In[233]:
+
+
 count = 0
 for k, i in zip(keywords, topic_idx):
     vectors = simple_sim[count]
     for j in i[:10]:
-        print('“%s” is about %s' % (k, topic_labels[j]))
+        print('“%s” is about %s' %(k, topic_labels[j]))
         print(vectors[j])
-    count = count + 1
+    count=count+1
+
